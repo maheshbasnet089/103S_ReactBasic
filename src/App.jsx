00c818265ed0d1affb474a94c6a1c22617f5a558
maheshbasnet089 <basnetmanish089@gmail.com>
2023-10-17 16:48:50 +0545

    learn useEffect and its types

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './Home'
 import About from './About'
+import UseEffect from './UseEffect'
 
 // hello world this is test 
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Home />}  />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<h1>contact page</h1>} />
+      <Route path="/useEffect" element={<UseEffect />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/UseEffect.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+const UseEffect = () => {
+    const [count,setCount] = useState(0)
+    const [count2,setCount2]  = useState(0)
+
+// TYPE 1 of UseEffect on the basis of execution
+// useEffect(()=>{
+
+//     // api bata data lyadey 
+//     console.log("Hello World")
+// },[])
+
+// Type 2 of UseEffect on the basis of execution
+// useEffect(()=>{
+//     document.title  = count
+//     console.log("useEffect Triggered")
+// },[count,count2])
+
+// Type 3 of useEffect on the basis of Execution
+
+const consoleLog = ()=>console.log("UseEffect Triggered")
+
+useEffect(()=>{
+    consoleLog()
+})
+
+
+  return (
+    <div>
+        <h1>Count : {count}</h1>
+        <button onClick={()=>setCount(count+1)}>+</button>
+        <h1>Count2 : {count2}</h1>
+        <button onClick={()=>setCount2(count2-1)}>-</button>
+    </div>
+  )
+}
+
+export default UseEffect
